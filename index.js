c2af494ea97ffb916bd655250ef85ebd0a3c10f4
nBappi <bappi.cse562@gmail.com>
2023-01-26 23:09:12 +0600

    Create state and action

---
+++ index.js
@@ -0,0 +1,21 @@
+// Create state 
+
+const increment = "INCREMENT"
+const decrement = "DECREMENT"
+const reset = "RESET"
+
+const initialState = {
+    counter : 0
+}
+
+const incrementAction = {
+    type : increment
+}
+
+const decrementAction = {
+    type : decrement
+}
+
+const resetAction = {
+    type : reset
+}
