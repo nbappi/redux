134e4ee39e5a2c1a9fadfcca2bd3a8731f65afb9
nBappi <bappi.cse562@gmail.com>
2023-01-27 00:44:43 +0600

    Multiple reducer

--- productCard.js
+++ productCard.js
@@ -1,12 +1,20 @@
 // Products
-const { createStore } = require("redux")
+const { createStore, combineReducers } = require("redux")
 
 const GET_PRODUCTS = "GET_PRODUCTS"
 const ADD_PRODUCT = "ADD_PRODUCT"
 
+const GET_CARTS = "GET_CARTS"
+const ADD_CART = "ADD_CART"
+
 const initProducts = {
     products : ["sugar", 'salt'],
     count : 2
+}
+
+const initCarts = {
+    carts : ["sugar"],
+    count : 1
 }
 
 const getProducts = () => {
@@ -18,6 +26,19 @@ const getProducts = () => {
 const addProduct = (name) => {
     return {
         type : ADD_PRODUCT,
+        payload : name
+    }
+}
+
+const getCarts = () => {
+    return {
+        type : GET_CARTS
+    }
+}
+
+const addCart = (name) => {
+    return {
+        type : ADD_CART,
         payload : name
     }
 }
@@ -36,11 +57,36 @@ const productReducer = ( state = initProducts, action ) => {
                 count : state.count + 1
             }
         default:
-            state
+            return state
     }
 }
 
-const store = createStore(productReducer);
+const cartReducer = ( state = initCarts, action ) => {
+
+    switch (action.type) {
+        case GET_CARTS:
+            return {
+                ...state
+            }
+
+        case ADD_CART:
+            return {
+                carts : [ ...state.carts, action.payload ],
+                count : state.count + 1
+            }
+        default:
+            return state
+    }
+}
+
+const rootReducer = combineReducers(
+    {
+        productR : productReducer,
+        cartR : cartReducer
+    }
+);
+
+const store = createStore(rootReducer);
 
 store.subscribe(() => {
     console.log(store.getState());
@@ -50,3 +96,6 @@ store.dispatch(getProducts());
 store.dispatch(addProduct("Water"));
 
 // Cards
+
+store.dispatch(getCarts());
+store.dispatch(addCart('pen'));
