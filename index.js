9f1eadb9d79cb63b0a184af210484f656225269f
nBappi <bappi.cse562@gmail.com>
2023-01-26 23:15:33 +0600

    create reducer

--- index.js
+++ index.js
@@ -25,3 +25,28 @@ const resetAction = () => {
         type : reset
     }
 }
+
+const counterReducer = ( state = initialState , action) =>{
+    switch (action.type) {
+        case increment :
+            
+        return {
+            ...state,
+            counter : state.counter + 1
+        }
+        case decrement :
+           return {
+            ...state,
+            counter : state.counter - 1 
+           }
+        case reset :
+
+        return {
+            ...state,
+            counter : 0
+        }
+    
+        default:
+           state;
+    }
+}
